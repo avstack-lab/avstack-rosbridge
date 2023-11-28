ca9f5906e22b8a4d6179ada5c9d183feeb82afff
Spencer Hallyburton <20426598+roshambo919@users.noreply.github.com>
2023-11-28 11:35:59 -0500

    Add tests for reference conversions

--- avstack_bridge/avstack_bridge/base.py
+++ avstack_bridge/avstack_bridge/base.py
@@ -8,9 +8,10 @@ from tf2_ros.buffer import Buffer
 
 
 class Bridge:
-    # def __init__(cls) -> None:
-    #     cls.tf_buffer = Buffer()
-    #     cls.tf_listener = TransformListener(cls.tf_buffer, cls)
+
+    ############################################
+    # TIME
+    ############################################
 
     @staticmethod
     def time_to_rostime(timestamp: float) -> Time:
@@ -25,14 +26,45 @@ class Bridge:
         ts = float(msg.sec + msg.nanosec / 1e9)
         return ts
 
+    ############################################
+    # REFERENCE -- to AVstack
+    ############################################
+
+    @classmethod
+    def header_to_reference(
+        cls,
+        header: Header | None,
+        tf_buffer: Buffer,
+        from_frame: str = "world",
+    ) -> ReferenceFrame:
+        if (header is None) or (header.frame_id == "world"):
+            reference = GlobalOrigin3D
+            if header is not None:
+                reference.timestamp = cls.rostime_to_time(header.stamp)
+        else:
+            reference = cls.frameid_to_reference(
+                to_frame=header.frame_id,
+                from_frame=from_frame,
+                time=header.stamp,
+                tf_buffer=tf_buffer,
+            )
+        return reference
+
     @classmethod
     def frameid_to_reference(
-        cls, to_frame: str, from_frame: str, time: Time
+        cls,
+        to_frame: str,
+        from_frame: str,
+        time: Time,
+        tf_buffer: Buffer,
     ) -> ReferenceFrame:
         tr = cls.frameid_to_tf2(to_frame=to_frame, from_frame=from_frame, time=time)
         if from_frame != "world":
             tran_world_2_from = cls.frameid_to_tf2(
-                to_frame=from_frame, from_frame="world", time=time
+                to_frame=from_frame,
+                from_frame="world",
+                time=time,
+                tf_buffer=tf_buffer,
             )
         else:
             tran_world_2_from = GlobalOrigin3D
@@ -59,9 +91,17 @@ class Bridge:
         )
         return ref
 
+    ############################################
+    # REFERENCE -- to ROS
+    ############################################
+
     @classmethod
     def frameid_to_tf2(
-        cls, tf_buffer: Buffer, to_frame: str, from_frame: str, time: Time
+        cls,
+        to_frame: str,
+        from_frame: str,
+        time: Time,
+        tf_buffer: Buffer,
     ) -> TransformStamped:
         try:
             tran = tf_buffer.lookup_transform(
@@ -76,30 +116,33 @@ class Bridge:
 
     @classmethod
     def reference_to_tf2(cls, reference: ReferenceFrame) -> Transform:
-        translation = Vector3(x=reference.x[0], y=reference.x[1], z=reference.z[2])
+        translation = Vector3(x=reference.x[0], y=reference.x[1], z=reference.x[2])
         rotation = Quaternion(
-            x=reference.q[0], y=reference.q[1], z=reference.q[2], w=reference.q[3]
+            x=reference.q.x, y=reference.q.y, z=reference.q.z, w=reference.q.w
         )
         return Transform(translation=translation, rotation=rotation)
 
     @classmethod
-    def reference_to_header(cls, reference: ReferenceFrame, timestamp: float) -> Header:
-        stamp = cls.time_to_rostime(timestamp)
+    def reference_to_tf2_stamped(cls, reference: ReferenceFrame) -> TransformStamped:
+        header = cls.reference_to_header(reference)
+        transform = cls.reference_to_tf2(reference)
+        return TransformStamped(
+            header=header, child_frame_id=reference.to_frame, transform=transform
+        )
+
+    @classmethod
+    def reference_to_header(
+        cls, reference: ReferenceFrame, timestamp: float | None = None
+    ) -> Header:
+        stamp = cls.time_to_rostime(
+            reference.timestamp if timestamp is None else timestamp
+        )
         frame_id = cls.reference_to_frameid(reference)
         return Header(stamp=stamp, frame_id=frame_id)
 
     @classmethod
     def reference_to_frameid(cls, reference: ReferenceFrame) -> str:
-        return reference.to_frame
-
-    @classmethod
-    def header_to_reference(
-        cls, header: Header | None, tf_buffer: Buffer
-    ) -> ReferenceFrame:
-        if (header is None) or (header.frame_id == "world"):
-            reference = GlobalOrigin3D
-            if header is not None:
-                reference.timestamp = cls.rostime_to_time(header.stamp)
+        if reference.is_global_origin:
+            raise RuntimeError("Not supposed to pass the global origin to this")
         else:
-            reference = cls.frameid_to_reference(header.frame_id, tf_buffer)
-        return reference
+            return reference.from_frame
--- avstack_bridge/tests/test_base_bridge.py
+++ avstack_bridge/tests/test_base_bridge.py
@@ -1,18 +1,10 @@
+import numpy as np
+from avstack.geometry import GlobalOrigin3D, ReferenceFrame
 from builtin_interfaces.msg import Time
+from geometry_msgs.msg import TransformStamped
+from std_msgs.msg import Header
 
 from avstack_bridge.base import Bridge
-
-
-# def test_ref_to_header():
-#     raise
-
-
-# def test_header_to_ref():
-#     raise
-
-
-# def test_tf2_from_header():
-#     raise
 
 
 def test_time_to_rostime_to_time():
@@ -22,3 +14,32 @@ def test_time_to_rostime_to_time():
     t_float2 = av_bridge.rostime_to_time(t_ros)
     assert isinstance(t_ros, Time)
     assert t_float == t_float2
+
+
+def test_reference_to_tf2_stamped():
+    base_bridge = Bridge()
+    ref = ReferenceFrame(
+        x=np.random.randn(3),
+        q=np.quaternion(*np.random.rand(3)),
+        reference=GlobalOrigin3D,
+        timestamp=1.01,
+        from_frame="world",
+        to_frame="frame1",
+    )
+    tf2 = base_bridge.reference_to_tf2_stamped(ref)
+    assert isinstance(tf2, TransformStamped)
+
+
+def test_reference_to_header():
+    base_bridge = Bridge()
+    ref = ReferenceFrame(
+        x=np.random.randn(3),
+        q=np.quaternion(*np.random.rand(3)),
+        reference=GlobalOrigin3D,
+        timestamp=1.01,
+        from_frame="world",
+        to_frame="frame1",
+    )
+    header = base_bridge.reference_to_header(ref)
+    assert isinstance(header, Header)
+    assert header.frame_id == "world"
